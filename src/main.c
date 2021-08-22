452e8575e1eb60f4a060395f742ce9e536604199
Caleb Adewole <caleberioluwa@gmail.com>
2021-08-22 14:21:54 +0100

    inital repo push

---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include "hash_table.h"
+
+int main()
+{
+    ht_table *ht = ht_new();
+    delete_ht_table(ht);
+}
---
+++ src/prime.c
@@ -0,0 +1,26 @@
+#include "prime.h"
+
+int is_prime(int n)
+{
+    if (n < 2)
+        return -1;
+    if (n < 4)
+        return 1;
+    if (n % 2 == 0)
+        return 0;
+    for (int i = 3; i * i <= n; i += 2)
+    {
+        if (n % i == 0)
+            return 0;
+    }
+    return 1;
+}
+
+int next_prime(int n)
+{
+    while (is_prime(n) != 1)
+    {
+        n++;
+    }
+    return n;
+}
